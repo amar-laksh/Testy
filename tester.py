8689da1099633f0b533434d038eda0902e79a2ee
amar-laksh <zacnethen@gmail.com>
2015-11-17 22:35:37 +0530

    Java Support initiated

--- tester.py
+++ tester.py
@@ -1,9 +1,20 @@
 import re
-kb = {
+Jkb = {
+        'import':r'[\t ]*?import[\t ]*?([a-zA-Z0-9\*\.]*);',
+        'varPriv':r'',
+        'varPub':r'',
+        'varPro':r'',
+        'varFinal':r'',
+        'var'
+    }
+Pykb = {
         'def':r'.*?def.*?\( *self *\) *?:$|.*?def.*?\( *\) *?:$',
         'defParam':r'.*?def.*?\(self\).?:.*?'
       }
-f = open('focusTV.py','r')
+f = open('../java-IDE/CodingWindow.java','r')
+
 for i in f.readlines():
-    if re.match(kb['def'],i):
-        print i
+    if re.match(Jkb['import'],i):
+        print re.findall(Jkb['import'],str(i))
+    
+    if re.match(Jkb[''])
